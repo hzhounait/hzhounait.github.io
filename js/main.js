4249fcf0b0de6c1e25335973bcb62b766d3f022d
Huang Zhou <hzhou2@ualberta.ca>
2018-12-19 10:26:13 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,118 +1,113 @@
-// Enter JavaScript for the exercise here...
-// add the dom load event
-// read in both them plates
-window.onload = function(){
-    const transactionForm = document.forms['frm-transactions'];
-    const creditTemplate = document.querySelector('template').content.children[0];
-    const debitTemplate = document.querySelector('template').content.children[1];
-    const recordContainer = document.querySelector('.transactions thead');
-    // console.log(recordContainer)
+$('document').ready(function(e){
+    let isLoaded = false;
 
-    // add and event listener to the form
-     transactionForm.addEventListener('submit', function(e){
-         // stop the default behaviour
-         e.preventDefault();
-         //create a validation object.
-         const newTransaction = getValidationObject(e.currentTarget);
+    if(!isLoaded){
+        isLoaded = true;
 
-        //  console.log(newTransaction.type);
+        let isPlayed = false;
 
-         // check the value of the type if its a credit or debit
-         // request the format 
-         if(newTransaction.type !== 'empty'){
-            transactionFormatter(newTransaction);
-            window.clearTimeout(timeoutHandle);
-            timeoutHandle = window.setTimeout(newCallBack, 120000);
-
-         }
-        
-       
-     })
-
-    function transactionFormatter(aTransaction) {
-
-        let template = null;
-        if(aTransaction.type === "credit"){
-            template = creditTemplate;
-        }else{
-            template = debitTemplate;
-        }
-
-        // console.log(template);       
-        // create a new transaction node
-        const newTemplate = template.cloneNode(true);
-        const descriptionField = newTemplate.querySelector('tr :nth-child(1)');
-        const typeField = newTemplate.querySelector('tr :nth-child(2)');
-        const amountField = newTemplate.querySelector('tr :nth-child(3)');
-        const removeButton = newTemplate.querySelector('tr :nth-child(4)');
-        descriptionField.textContent = aTransaction.description;
-        typeField.textContent = aTransaction.type
-        amountField.textContent = aTransaction.amount;
-        removeButton.addEventListener('click', function (e) {
-            recordContainer.removeChild(e.currentTarget.parentNode);
-            updateTotals();
-            window.clearTimeout(timeoutHandle);
-            timeoutHandle = window.setTimeout(newCallBack, 120000);
-        })
-        
-        recordContainer.appendChild(newTemplate);
-        updateTotals()
-
-
-    }
-
-   
-    function updateTotals(){
-        let creditTotal = 0;
-        let debitTotal = 0;
-        const records = Array.from(recordContainer.children);
-        // console.log(records)
-        records.forEach(function(elm){          
-            let type = elm.classList.value;           
-            if(type === "credit"){
-                creditTotal = creditTotal + Number(elm.querySelector('.amounts').textContent);             
-            }else if(type === "debit"){
-                debitTotal = debitTotal + Number(elm.querySelector('.amounts').textContent);
-            }else{
-                console.log("null")
-            }
-            
-            
-        })
-
-        recordContainer.querySelector('.debits').innerHTML = "$" + debitTotal.toFixed(2)
-        recordContainer.querySelector('.credits').innerHTML = "$" + creditTotal.toFixed(2)
-
-    }
-
-    function getValidationObject(formData) {
-        const description = document.getElementById('description').value;
-        const type = document.getElementById('type').value;
-        const amount = document.getElementById('amount').value;
-        const newTransactionObject = {
-            description,
-            type,
-            amount
-        }
-        return newTransactionObject
-
-    }
-
-
-    var timeoutHandle = window.setTimeout(newCallBack, 120000);  
-
-    function newCallBack(){
-        alert("Your Session Time End");
-        const records = Array.from(recordContainer.children);
-        records.forEach(function(elm){
-            let type = elm.classList.value; 
-            if(type === "credit" || type === "debit"){
-                elm.remove();
-                
-                recordContainer.querySelector('.credits').innerHTML = "$0.00"
-                recordContainer.querySelector('.debits').innerHTML = "$0.00"
-
+        $('#musicPlay').click(function () {
+            $('i', this).toggleClass("fa-volume-up fa-volume-off");
+            if (!isPlayed) {
+                isPlayed = true;
+                console.log(document.querySelector('audio'))
+                document.querySelector('audio').pause();
+                document.querySelector('audio').currentTime = 0;
+            } else {
+                isPlayed = false;
+                document.querySelector('audio').play();
             }
         })
+
+        $.getJSON('json/slides.json', function(data){
+
+            const defaultDisplay = 
+            `
+            <li>
+                <img src="img/${data[0].img}" class="photo" />
+            </li>
+            <li>
+                <span class="annotation">${data[0].annotation}</span>
+            </li>
+            `
+            $('.slideshowul').html(defaultDisplay);
+
+
+            let count = data.length;
+            let dataId = Number(document.querySelector('.btnNext').dataset.id);
+            $('.btnNext').click(function(e){
+                if(dataId < count - 1){
+                    dataId += 1;                 
+                    document.querySelector('.btnNext').dataset.id = dataId.toString();
+                    console.log(document.querySelector('.btnNext').dataset.id)
+                    console.log(e.currentTarget)
+
+                    if(e.currentTarget.dataset.id != 0){
+                        const display = 
+                        `
+                        <li>
+                            <img src="img/${data[e.currentTarget.dataset.id].img}" class="photo" />
+                        </li>
+                        <li>
+                            <span class="annotation">${data[e.currentTarget.dataset.id].annotation}</span>
+                        </li>
+                        `     
+                       
+                       $('.slideshowul').html(display);
+                    }
+                }
+                else{
+                    console.log("overlimit")
+                    $('.next').hide();
+                    const newBtn = 
+                    `
+                    <span class="yes"><a class="btnYes" href="#">Yes I Do</a></span>
+                    <span class="no"><a class="btnNo" href="#">No I Don't</a></span>
+                    `
+                    $('.btnGroup').html(newBtn);
+
+                    let count = 0;
+                    $('.btnNo').click(function(){
+                        count++;
+                        if (count <= 1){
+                            const display = 
+                            `
+                            <li>
+                                <span class="annotation">不再考虑一下吗！(　´・◡・｀)</span>
+                            </li>
+                            `  
+                             $('.slideshowul').html(display);
+                        }else{
+                            const display = 
+                            `
+                            <li>
+                                <span class="annotation">按钮好像失效了呢，试试Yes鸭 (°ཀ°)</span>
+                            </li>
+                            `  
+                             $('.slideshowul').html(display);
+                        }
+                        
+                    })
+
+                    $('.btnYes').click(function(){
+                        const display = 
+                        `
+                        <li>
+                            <img src="img/jane_doe.jpg" class="photo" />
+                        </li>
+                        <li>
+                            <span class="annotation">恭喜你不仅收获了一个泰迪熊玩偶，你还收获了一个大猪蹄子？！惊不惊喜？意不意外？原谅我用这种方式与你告白，套路也好，土味也罢！但是我是真的喜欢你鸭！</span>
+                        </li>
+                        `     
+                       
+                       $('.slideshowul').html(display);
+                       $('.btnGroup').hide();
+
+                    })
+                }
+            })
+        })
+    }else{
+        isLoaded = false;
     }
-}
+})
